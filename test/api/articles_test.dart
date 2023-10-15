122730878a8c1728887555437f57b21190e94466
Max Weber <weber.max.1988@gmail.com>
2023-10-15 20:11:26 +0200

    Fixed a test and removed the packages folder from the repository

--- test/api/articles_test.dart
+++ test/api/articles_test.dart
@@ -13,7 +13,7 @@ void main() {
           strapiKey:
               "56f64dcbfb2ad57fee935976895de9134b599c46cb524b8ceecc7272fae332795a73c56d369ebe81958500196a7dd0cca3efa211a2e94b2490026c4018e41151806d679f657c969d7646dfec85692524b48419436a0820a608928af9444d796d18e2251fb3426f403b9428682167ac30d417b12808ce89d9bef0717103d1e4c9",
           strapiUrl: "http://localhost:1337/api");
-      var result = await subject.getArticle(env);
+      var result = await subject.getArticle(env, 1);
       expect(result, isA<Map<String, dynamic>>());
       expect(result.length, 2);
       print(const JsonEncoder().convert(result));
